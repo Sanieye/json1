549b5ec2a1fdad795f3bf08a806dfb7c5b1ae725
Sanieye <88657760+Sanieye@users.noreply.github.com>
2021-12-07 13:31:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,38 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
 </head>
 <body>
- 
     <table>
         <thead>
             <tr>
-                <th>ID</th><th>First Name</th><th>Last Name</th>
+                <th>ID</th><th>Title</th><th>Author</th>
             </tr>
         </thead>
-        <tbody id="tblStudent">
+        <tbody id="tblPosts">
         </tbody>
     </table>
 </body>
 <script>
- 
-function loadJSON(){
-    var url = "https://cj-android-demon.herokuapp.com/json2.php";
+    function loadJSON(){
+    var url = "https://jsonplaceholder.typicode.com/posts";
 
     $.getJSON(url)
         .done((data)=>{
             console.log(data);
             $.each(data, (k, item)=>{
-                console.log(k);
                 console.log(item);
-
-
-                var line = "<tr>";
-                    line += "<td>" + (k+1+ "</td>");
-                    line += "<td>" + item.fname + "</td>";
-                    line += "<td>" + item.lname +  "</td>";
-                    line += "</tr>" ;
-                $("#tblStudent").append(line);
             });
 })
         .fail((xhr, status, err)=>{
@@ -48,5 +38,6 @@ function loadJSON(){
 $(()=>{
     loadJSON();
 });
+
 </script>
 </html>
