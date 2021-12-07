6505dbfa500ee42844e99d805367e428ed504a23
Sanieye <88657760+Sanieye@users.noreply.github.com>
2021-12-07 15:57:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,109 +1,113 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
 
 </head>
+
 <body>
-<button id="btnBack"> back </button>
-<div id="main">
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th> <th>Title</th><th> Details </th>
-            </tr>
-        </thead>
-        <tbody id="tblPosts">
-        </tbody>
-    </table>
-</div>
+    <button id="btnBack"> back </button>
+    <div id="main">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th> Details </th>
+                </tr>
+            </thead>
+            <tbody id="tblPosts">
+            </tbody>
+        </table>
+    </div>
 
-<div id="detail">
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th> <th>Title</th><th> UserId</th>
-            </tr>
-        </thead>
-        <tbody id="tblDetails">
-        </tbody>
-    </table> 
-    
-</div>
+    <div id="detail">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th> UserId</th>
+                </tr>
+            </thead>
+            <tbody id="tblDetails">
+            </tbody>
+        </table>
 
-    
+    </div>
+
+
 
 
 </body>
 
 <script>
-    function showDetails(id){
+    function showDetails(id) {
         $("#main").hide();
         $("#detail").show();
-        var url = "https://jsonplaceholder.typicode.com/posts/"+id;
+        var url = "https://jsonplaceholder.typicode.com/posts/" + id;
         $.getJSON(url)
-            .done((data)=>{
+            .done((data) => {
                 console.log(data);
                 var line = "<tr id='rowdetail'>";
-                        line += "<td>"+ data.id + "</td>";
-                        line += "<td><b>"+ data.title + "</b><br/>";
-                        line += data.body + "</td>";
-                        line += "<td>"+ data.uderid + "</td>";
-                        line += "</tr>";
-                   
+                line += "<td>" + data.id + "</td>";
+                line += "<td><b>" + data.title + "</b><br/>";
+                line += data.body + "</td>";
+                line += "<td>" + data.userid + "</td>";
+                line += "</tr>";
+
 
             })
-            .fail((xhr, status, error)=>{
+            .fail((xhr, status, error) => {
 
             })
     }
-    function loadPosts(){
+    function loadPosts() {
         $("#main").show();
         $("#details").hide();
-        
+
         var url = "https://jsonplaceholder.typicode.com/posts";
         $.getJSON(url)
-            .done((data)=>{
-                $.each(data, (k, item)=>{
+            .done((data) => {
+                $.each(data, (k, item) => {
                     console.log(item);
                     var line = "<tr>";
-                        line += "<td>"+ item.id + "</td>";
-                        line += "<td><b>"+ item.title + "</b><br/>";
-                        line += item.body + "</td>";
-                        line += "<td> <button onClick='showDetails("+ item.id +");' > link </button> </td>";
+                    line += "<td>" + item.id + "</td>";
+                    line += "<td><b>" + item.title + "</b><br/>";
+                    line += item.body + "</td>";
+                    line += "<td> <button onClick='showDetails(" + item.id + ");' > link </button> </td>";
 
-                        line += "</tr>";
+                    line += "</tr>";
                     $("#tblPosts").append(line);
                 });
                 $("#main").show();
             })
-            .fail((xhr, status, error)=>{
+            .fail((xhr, status, error) => {
 
             })
     }
-  
-    $(()=>{
+
+    $(() => {
 
         loadPosts();
-        $("#btnBack").click(()=>{
+        $("#detail").hide();
+        $("#btnBack").click(() => {
             $("#main").show();
             $("#detail").hide();
-            $("#rowdetail").clear();
-           
-       
+
+
+
         });
-    
+
     })
 
 </script>
-
-
-
-
-
+</html>
