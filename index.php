ad382b6d8e5f299724bb4307caa3194f9d58cb2f
Sanieye <88657760+Sanieye@users.noreply.github.com>
2021-12-07 11:19:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-sanita
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+</head>
+<body>
+    <button id="btnJSON1">JSON1</button>
+    name : <span id="fname"></span> <br/>
+    sname : <span id="lname"></span> 
+    
+</body>
+<script>
+function loadJSON(){
+        var url = "http://cj-android-demon.herokuapp.com/json2.php";
+  
+     $.getJSON(url).DONE((data)=>{
+            console.log(data);
+            $("#fname").text(data.fname);
+
+        })
+        .fail((xhr,status,err)=>{
+
+        });
+}
+$(()=>{
+     $("btnJSON1").click(loadJSON);
+});
+</script>
+</html>
